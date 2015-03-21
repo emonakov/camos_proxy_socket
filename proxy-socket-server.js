61e3ba48d834c3b173ed074970c9d00ca5a7dca9
madgals <madgals@mail.ru>
2015-03-21 23:48:30 +0300

    standalone

--- proxy-socket-server.js
+++ proxy-socket-server.js
@@ -1,24 +1,16 @@
 var socket = require('socket.io');
 var WebSocket = require('ws');
-var http = require('http');
+
 var conf = require('./lib/config');
 
-var app = http.createServer();
-var io = socket.listen(app.listen(conf.get('port'), conf.get('host')));
-
-app.on('request', function (req, res) {
-    res.statusCode = 404;
-    res.end();
-});
+var io = socket.listen(conf.get('port'), conf.get('host'));
 
 io.on('connection', function (socket) {
     var ws;
     socket.on('auth', function (params) {
         ws = new WebSocket(conf.get('socket'));
         try {
-            var tmpParams = JSON.parse(params);
             ws.on('message', function (message) {
-                var conn = socket;
                 socket.emit('message', message);
             });
             ws.on('error', function (err) {
@@ -29,6 +21,9 @@ io.on('connection', function (socket) {
                 var pingWsInterval = setInterval(function () {
                     ws.send('{"opt":"system.ping", "data":null}');
                 }, 15000);
+                socket.on('disconnect', function () {
+                    clearInterval(pingWsInterval);
+                });
             });
         } catch (error) {
             this.emit('error', error);
