ef596a58c847682b084ad14182a7463fb6e2fcc9
madgals <madgals@mail.ru>
2015-03-22 11:57:39 +0300

    fix

--- proxy-socket-server.js
+++ proxy-socket-server.js
@@ -7,31 +7,26 @@ var io = socket.listen(conf.get('port'), conf.get('host'));
 
 io.on('connection', function (socket) {
     var ws;
-    socket.on('auth', function (params) {
-        ws = new WebSocket(conf.get('socket'));
-        try {
-            ws.on('message', function (message) {
-                socket.emit('message', message);
-            });
-            ws.on('error', function (err) {
-                socket.emit('error', err);
-            });
-            ws.on('open', function () {
-                ws.send(params);
-                var pingWsInterval = setInterval(function () {
-                    ws.send('{"opt":"system.ping", "data":null}');
-                }, 15000);
-                socket.on('disconnect', function () {
-                    clearInterval(pingWsInterval);
-                });
-            });
-        } catch (error) {
-            this.emit('error', error);
-        }
-    });
     socket.on('message', function (params) {
         try {
-            ws.send(params);
+            var data = JSON.parse(params);
+            if (data.opt === "user.auth.token") {
+                ws = new WebSocket(conf.get('socket'));
+                ws.on('message', function (message) {
+                    socket.emit('message', message);
+                });
+                ws.on('open', function () {
+                    ws.send(params);
+                    var pingWsInterval = setInterval(function () {
+                        ws.send('{"opt":"system.ping", "data":null}');
+                    }, 15000);
+                    socket.on('disconnect', function () {
+                        clearInterval(pingWsInterval);
+                    });
+                });
+            } else {
+                ws.send(params);
+            }
         } catch (error) {
             this.emit('error', error);
         }
