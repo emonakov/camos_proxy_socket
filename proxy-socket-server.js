07d73459fe3a175a00b131bf75a48c51195343fb
madgals <evgeniy.monakov@ya.ru>
2015-03-23 16:47:24 +0300

    fix

--- proxy-socket-server.js
+++ proxy-socket-server.js
@@ -4,27 +4,29 @@ var app = require('http').createServer();
 var conf = require('./lib/config');
 
 var io = socket.listen(app.listen(conf.get('port'), conf.get('host')));
+
 app.on('request', function (req, res) {
-    res.statusCode = 404;
-    res.end();
+    //res.statusCode = 404;
+    //res.end();
 });
+
 io.on('connection', function (socket) {
     var ws;
     socket.on('message', function (params) {
+        console.log(params);
         try {
             var data = JSON.parse(params);
             if (data.opt === "user.auth.token") {
                 ws = new WebSocket(conf.get('socket'));
                 ws.on('message', function (message) {
+                    console.log(message);
                     socket.emit('message', message);
                 });
                 ws.on('open', function () {
                     ws.send(params);
-                    var pingWsInterval = setInterval(function () {
-                        ws.send('{"opt":"system.ping", "data":null}');
-                    }, 15000);
                     socket.on('disconnect', function () {
-                        clearInterval(pingWsInterval);
+                        console.log('disconnect');
+                        ws.close();
                     });
                 });
             } else {
