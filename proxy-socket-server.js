dda201ce416f293c4e9b1ccf79757c94ef0a6ef6
madgals <evgeniy.monakov@ya.ru>
2015-03-25 10:38:03 +0300

    refactor

--- proxy-socket-server.js
+++ proxy-socket-server.js
@@ -12,31 +12,33 @@ app.on('request', function (req, res) {
 
 io.on('connection', function (socket) {
     var ws;
+    socket.on('auth', function (params) {
+        try {
+            ws = new WebSocket(conf.get('socket'));
+            ws.on('message', function (message) {
+                console.log(message);
+                socket.emit('message', message);
+            });
+            ws.on('open', function () {
+                ws.send(JSON.stringify(params));
+            });
+        } catch (err) {
+            this.emit('error', error);
+        }
+    });
     socket.on('message', function (params) {
         console.log(params);
         try {
-            var data = JSON.parse(params);
-            if (data.opt === "user.auth.token") {
-                ws = new WebSocket(conf.get('socket'));
-                ws.on('message', function (message) {
-                    console.log(message);
-                    socket.emit('message', message);
-                });
-                ws.on('open', function () {
-                    ws.send(params);
-                    socket.on('disconnect', function () {
-                        console.log('disconnect');
-                        ws.close();
-                    });
-                });
-            } else {
-                ws.send(params);
-            }
+            ws.send(params);
         } catch (error) {
             this.emit('error', error);
         }
     });
     socket.on('error', function (error) {
         this.emit(error);
+    });
+    socket.on('disconnect', function () {
+        console.log('disconnect');
+        ws.close();
     });
 });
